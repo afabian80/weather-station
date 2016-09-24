a5f3f5f8604e597d6e2f4ec7b6f8de8b835a6296
Akos Fabian <fabian.akos@gmail.com>
2016-09-24 15:13:09 +0200

    Update api.key filename

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 BASEDIR=$(dirname "$0")
-KEY=`cat $BASEDIR/darksky.key`
+KEY=`cat $BASEDIR/api.key`
 COORD=47.55,19.0434 # Budapest
 #COORD=53.5259,-7.3381 # Ireland, raining now
 
