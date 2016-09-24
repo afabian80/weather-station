8eef2a97a02d9d5fa080d7cbb2d071054ab738df
Akos Fabian <fabian.akos@gmail.com>
2016-09-24 15:05:44 +0200

    Hide the api key

--- update.sh
+++ update.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -ex
 
 BASEDIR=$(dirname "$0")
-KEY=c26a1b3b3045e18845d765446a523e73
+KEY=`cat $BASEDIR/darksky.key`
 COORD=47.55,19.0434 # Budapest
-#COORD=51.5074,0.1278 # London
 #COORD=53.5259,-7.3381 # Ireland, raining now
 
-curl -s -G -o ${BASEDIR}/data.json -d units=si -d exclude=minutely,hourly,daily,flags https://api.darksky.net/forecast/${KEY}/${COORD}
+curl -s -G \
+	-o ${BASEDIR}/data.json \
+	-d units=si \
+	-d exclude=minutely,hourly,daily,flags \
+	https://api.darksky.net/forecast/${KEY}/${COORD}
 
 
