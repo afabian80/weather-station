8974e78d6267347feca6dbc586f5aa8f6f3258dc
Akos Fabian <fabian.akos@gmail.com>
2016-09-24 14:44:06 +0200

    KEY and COORD extracted

--- update.sh
+++ update.sh
@@ -1,8 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 BASEDIR=$(dirname "$0")
-COORD=47.55,19.0434
+KEY=c26a1b3b3045e18845d765446a523e73
+COORD=47.55,19.0434 # Budapest
+#COORD=51.5074,0.1278 # London
+#COORD=53.5259,-7.3381 # Ireland, raining now
 
-curl -s -G -o ${BASEDIR}/data.json -d units=si -d exclude=minutely,hourly,daily,flags https://api.darksky.net/forecast/a4aa7a5cdb1f7fd0b6251179bd9ca7f5/${COORD}
+curl -s -G -o ${BASEDIR}/data.json -d units=si -d exclude=minutely,hourly,daily,flags https://api.darksky.net/forecast/${KEY}/${COORD}
 
 
